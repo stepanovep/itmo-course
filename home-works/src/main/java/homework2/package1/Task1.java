e29ce69943eb202e4663bb13593f29aa1fb6eaca
Egor Stepanov <stepanovep@gmail.com>
2017-10-13 20:53:57 +0300

    HomeWork2 - package1

---
+++ home-works/src/main/java/homework2/package1/Task1.java
@@ -0,0 +1,22 @@
+package homework2.package1;
+
+import java.util.Random;
+
+/**
+ * Вывести на экран число четное или нечетное
+ *
+ * @author Egor Stepanov
+ * @since 13/10/2017.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int n = rnd.nextInt() % 1000;
+        System.out.println("Сгенерировано число n = " + n);
+        System.out.println(isOdd(n) ? "Четный" : "Нечетный");
+    }
+
+    private static boolean isOdd(int n) {
+        return (n & 1) == 0;
+    }
+}
---
+++ home-works/src/main/java/homework2/package1/Task3.java
@@ -0,0 +1,36 @@
+package homework2.package1;
+
+import java.util.Scanner;
+
+/**
+ * Корни квадратного уравнения
+ *
+ * @author Egor Stepanov
+ * @since 13/10/2017.
+ */
+public class Task3 {
+
+    private static final double EPS = 1e-6;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        double c = in.nextDouble();
+
+        System.out.println(String.format("Given equation: %.2fx^2 + %.2fx + %.2f", a, b, c));
+        double d = b*b - 4*a*c;
+        if (d < 0) {
+            System.out.println("No roots of equation");
+        } else if (Math.abs(d) < EPS) {
+            double root = -b / (2*a);
+            System.out.println(String.format("The equation has one root: %.2f", root));
+        } else {
+            double root1 = (-b + Math.sqrt(d)) / (2*a);
+            double root2 = (-b - Math.sqrt(d)) / (2*a);
+            System.out.println(String.format(
+                    "The equation has two roots: root1=%.2f,  root2=%.2f", root1, root2));
+        }
+    }
+}
