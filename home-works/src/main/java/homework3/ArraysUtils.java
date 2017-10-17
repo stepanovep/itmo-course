c1663bfd2eb91d3c47ac2cefbe6f74d87b9cfc61
stepanovep <stepanovep@gmail.com>
2017-10-17 23:05:26 +0300

    Homework #3: 2d-arrays

--- home-works/src/main/java/homework3/ArraysUtils.java
+++ home-works/src/main/java/homework3/ArraysUtils.java
@@ -1,6 +1,5 @@
 package homework3;
 
-import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -10,7 +9,7 @@ import java.util.Random;
 public class ArraysUtils {
 
     /**
-     * Сгенерировать случайный массив из целых чисел
+     * Сгенерировать случайный массив из случайных целых чисел
      *
      * @param n размер массива
      * @param min минимальное значение
@@ -28,16 +27,47 @@ public class ArraysUtils {
     }
 
     /**
+     * Сгенерировать двумерный массив из случайных целых чисел
+     */
+    public static int[][] generate2dArray(int n, int m, int min, int max) {
+        Random rnd = new Random();
+        int[][] a = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            a[i] = generateArray(m, min, max);
+        }
+
+        return a;
+    }
+
+    /**
      * Вывести массив в стандартный вывод с префиксом
      */
     public static void printArray(String message, int ... a) {
-        System.out.println(message + ": "+ Arrays.toString(a));
+        if (message != null) {
+            System.out.println(message + ":");
+        }
+        System.out.print("[ ");
+        for (int i: a) {
+            System.out.print(i);
+            System.out.print('\t');
+        }
+        System.out.println(']');
     }
 
     /**
      * Вывести массив в стандартный вывод
      */
     public static void printArray(int ... a) {
-        System.out.println(Arrays.toString(a));
+        printArray(null, a);
+    }
+
+    /**
+     * Вывести двумерный массив в стандартный вывод
+     */
+    public static void printArray(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            printArray(a[i]);
+        }
+        System.out.println();
     }
 }
